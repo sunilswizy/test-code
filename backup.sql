486530a7bd2bb90b99f3c9b5458d59539ec7944a
Iamneo <techadmin@iamneo.ai>
2023-11-22 11:09:09 +0000

    Backup Commit

---
+++ backup.sql
@@ -0,0 +1,257 @@
+-- MariaDB dump 10.19  Distrib 10.6.14-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: appdb
+-- ------------------------------------------------------
+-- Server version	5.6.51
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `appdb`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `appdb` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `appdb`;
+
+--
+-- Table structure for table `player`
+--
+
+DROP TABLE IF EXISTS `player`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `player` (
+  `id` bigint(20) NOT NULL,
+  `age` int(11) NOT NULL,
+  `biddingprice` double NOT NULL,
+  `category` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `sold` bit(1) NOT NULL,
+  `team_id` bigint(20) DEFAULT NULL,
+  `teamid` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKdvd6ljes11r44igawmpm1mc5s` (`team_id`),
+  KEY `FKkn30qa9ck5eru9jh3g8x4tqi5` (`teamid`),
+  CONSTRAINT `FKdvd6ljes11r44igawmpm1mc5s` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`),
+  CONSTRAINT `FKkn30qa9ck5eru9jh3g8x4tqi5` FOREIGN KEY (`teamid`) REFERENCES `team` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `player`
+--
+
+LOCK TABLES `player` WRITE;
+/*!40000 ALTER TABLE `player` DISABLE KEYS */;
+INSERT INTO `player` VALUES (1000,24,0,'Seniorteam','Viratdemo@gmail.com','Demo','\0',NULL,NULL);
+/*!40000 ALTER TABLE `player` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `player_team`
+--
+
+DROP TABLE IF EXISTS `player_team`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `player_team` (
+  `player_id` bigint(20) NOT NULL,
+  `team_id` bigint(20) NOT NULL,
+  UNIQUE KEY `UK_gu8wic4w2582wn9ulicnnchsv` (`team_id`),
+  KEY `FKi5rpfhkcbhcotndp7k623f4y0` (`player_id`),
+  CONSTRAINT `FK9axcbkwl4aiy4b9stqe31q2k` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`),
+  CONSTRAINT `FKi5rpfhkcbhcotndp7k623f4y0` FOREIGN KEY (`player_id`) REFERENCES `player` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `player_team`
+--
+
+LOCK TABLES `player_team` WRITE;
+/*!40000 ALTER TABLE `player_team` DISABLE KEYS */;
+/*!40000 ALTER TABLE `player_team` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `players`
+--
+
+DROP TABLE IF EXISTS `players`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `players` (
+  `id` bigint(20) NOT NULL,
+  `age` int(11) NOT NULL,
+  `biddingprice` double NOT NULL,
+  `category` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `sold` bit(1) NOT NULL,
+  `teamid` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKk96osfe3mlbfwcv62lpj2gojr` (`teamid`),
+  CONSTRAINT `FKk96osfe3mlbfwcv62lpj2gojr` FOREIGN KEY (`teamid`) REFERENCES `teams` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `players`
+--
+
+LOCK TABLES `players` WRITE;
+/*!40000 ALTER TABLE `players` DISABLE KEYS */;
+INSERT INTO `players` VALUES (1000,24,0,'Seniorteam','Viratdemo@gmail.com','Demo','\0',NULL),(11111,34,8000000,'Seniorteam','Viratk@gmail.com','kohli','',NULL),(12231,34,8000000,'Seniorteam','Viratk@gmail.com','kohli','',NULL);
+/*!40000 ALTER TABLE `players` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `team`
+--
+
+DROP TABLE IF EXISTS `team`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `team` (
+  `id` bigint(20) NOT NULL,
+  `maximim_budget` double NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `maximum_budget` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `team`
+--
+
+LOCK TABLES `team` WRITE;
+/*!40000 ALTER TABLE `team` DISABLE KEYS */;
+INSERT INTO `team` VALUES (1000,0,'Demo',15000);
+/*!40000 ALTER TABLE `team` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `team_list`
+--
+
+DROP TABLE IF EXISTS `team_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `team_list` (
+  `team_id` bigint(20) NOT NULL,
+  `list_id` bigint(20) NOT NULL,
+  UNIQUE KEY `UK_9ftifpwwok61192095mky94cm` (`list_id`),
+  KEY `FKet8t2y3rupnfatag4whg8ew14` (`team_id`),
+  CONSTRAINT `FKet8t2y3rupnfatag4whg8ew14` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`),
+  CONSTRAINT `FKs2c2r1jlwam3iv2wb2fgk8juy` FOREIGN KEY (`list_id`) REFERENCES `player` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `team_list`
+--
+
+LOCK TABLES `team_list` WRITE;
+/*!40000 ALTER TABLE `team_list` DISABLE KEYS */;
+/*!40000 ALTER TABLE `team_list` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `team_players`
+--
+
+DROP TABLE IF EXISTS `team_players`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `team_players` (
+  `team_id` bigint(20) NOT NULL,
+  `players_id` bigint(20) NOT NULL,
+  UNIQUE KEY `UK_j7gm7vpm2kn3c07shxqlvt4al` (`players_id`),
+  KEY `FKc9igy2kys82rwa80px3q0usqa` (`team_id`),
+  CONSTRAINT `FKc9igy2kys82rwa80px3q0usqa` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`),
+  CONSTRAINT `FKoh3nyypbfjy208372s2aq12ei` FOREIGN KEY (`players_id`) REFERENCES `player` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `team_players`
+--
+
+LOCK TABLES `team_players` WRITE;
+/*!40000 ALTER TABLE `team_players` DISABLE KEYS */;
+/*!40000 ALTER TABLE `team_players` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `teams`
+--
+
+DROP TABLE IF EXISTS `teams`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `teams` (
+  `id` bigint(20) NOT NULL,
+  `maximum_budget` double NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teams`
+--
+
+LOCK TABLES `teams` WRITE;
+/*!40000 ALTER TABLE `teams` DISABLE KEYS */;
+INSERT INTO `teams` VALUES (111,30000,'chenn'),(1000,15000,'Demo');
+/*!40000 ALTER TABLE `teams` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `password` varchar(255) DEFAULT NULL,
+  `role` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1001 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'admin123','admin','admin'),(1000,'admin123','ADMIN','admin');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-22 11:06:49
